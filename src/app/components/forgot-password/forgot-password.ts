4d62ac0702c689440134061ed89070f7cbfece37
Your Name <you@example.com>
2025-06-12 23:44:51 +0200

    built forgot-password and reset-password

--- src/app/components/forgot-password/forgot-password.ts
+++ src/app/components/forgot-password/forgot-password.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/components/header/header';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './forgot-password.html',
-  styleUrl: './forgot-password.scss'
+  styleUrl: './forgot-password.scss',
 })
-export class ForgotPassword {
-
-}
+export class ForgotPassword {}
--- src/app/components/reset-password/reset-password.ts
+++ src/app/components/reset-password/reset-password.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/components/header/header';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './reset-password.html',
-  styleUrl: './reset-password.scss'
+  styleUrl: './reset-password.scss',
 })
-export class ResetPassword {
-
-}
+export class ResetPassword {}
