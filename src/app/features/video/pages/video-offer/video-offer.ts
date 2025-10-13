de6774b16e5ea0b88a353d1bfa6989ed784fb7fc
Your Name <you@example.com>
2025-10-14 00:03:57 +0200

    implemented video preview logic

--- src/app/features/video/pages/video-offer/video-offer.ts
+++ src/app/features/video/pages/video-offer/video-offer.ts
@@ -1,24 +1,21 @@
+import { ViewportScroller } from '@angular/common';
 import {
   Component,
   computed,
   ElementRef,
   inject,
   OnInit,
-  QueryList,
   signal,
   ViewChild,
-  ViewChildren,
-  WritableSignal,
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AuthStore, UserClient } from '@core/auth/services';
 
+import { AuthStore, UserClient } from '@core/auth/services';
 import { Footer } from '@core/layout/components';
 import { VideoHeader } from '@features/video/components';
-import { VideoGroup, VideoGroupData } from '@features/video/interfaces';
+import { VideoGroup } from '@features/video/interfaces';
 import { Video } from '@features/video/models';
-import { VideoStore } from '@features/video/services';
 import { Button } from '@shared/components/buttons';
 
 /**
@@ -33,87 +30,36 @@ import { Button } from '@shared/components/buttons';
 export class VideoOffer implements OnInit {
   private route: ActivatedRoute = inject(ActivatedRoute);
   private router: Router = inject(Router);
+  private scroller = inject(ViewportScroller);
   private auth = inject(AuthStore);
+
+  // review video header ...
+  // review video footer ...
+
+  // use newest video library[0].videos[0] ... ?
+
+  // review ...
   private user: UserClient = inject(UserClient);
-  private vs: VideoStore = inject(VideoStore);
-
-  // horizontal navigation menu: left scroll right ...
-  // mobile scrolling by swiping (overflow-x: auto) ...
-  // show UI only on container hover ...
-  // finalize UI navigation style ...
-
-  // better: native scroll bar with custom styling ... ?
 
   data = toSignal(this.route.data);
   library = computed(() => this.data?.()?.['result'] as VideoGroup[]);
 
-  libraryData: WritableSignal<VideoGroupData[]> = signal([]);
-  // library: WritableSignal<VideoGroup[]> = signal([]);
+  // review ...
+  newVideos = computed(() => this.library()[0].videos);
 
-  title: WritableSignal<string> = signal('Mighty Magic');
-  description: WritableSignal<string> = signal('Magic is powerful');
+  video = signal<Video | null>(null);
+  id = computed(() => this.video()?.id);
+  title = computed(() => this.video()?.title);
+  description = computed(() => this.video()?.description);
+  previewClip = computed(() => this.video()?.previewClip);
 
-  video?: WritableSignal<Video>;
-  videoId: number = 0;
-
-  currentTranslation: WritableSignal<number[]> = signal([]);
-  leftStop: WritableSignal<number[]> = signal([]);
-
-  scrollData: WritableSignal<
-    {
-      delta: number;
-      track: number;
-      indicator: number;
-      trackPercent: number;
-    }[]
-  > = signal([]);
-
-  scrollArea = signal(false);
-
+  // use source instead ... ?
   @ViewChild('preview') preview?: ElementRef<HTMLVideoElement>;
 
-  @ViewChildren('genreRowTrack') genreRowTrack?: QueryList<
-    ElementRef<HTMLDivElement>
-  >;
-  @ViewChildren('genreRowButton') genreRowButton?: QueryList<
-    ElementRef<HTMLButtonElement>
-  >;
-  @ViewChildren('genreRow') genreRow?: QueryList<ElementRef<HTMLDivElement>>;
-
+  // edit ...
   ngOnInit() {
-    window.addEventListener('resize', (event) => {
-      this.leftStop.update((values) =>
-        values.map((v, i) => {
-          const track =
-            this.genreRowTrack?.get(i)?.nativeElement.clientWidth ?? 0;
-          const indicator =
-            this.genreRow?.get(i)?.nativeElement.scrollWidth ?? 0;
-          console.log('delta: ', track - indicator);
+    this.setRandomVideo();
 
-          return track - indicator;
-        })
-      );
-    });
-
-    this.preview?.nativeElement.setAttribute('type', 'video/mp4');
-    this.preview?.nativeElement.setAttribute(
-      'src',
-      this.library()[0].videos[0].previewClip
-    );
-    this.preview?.nativeElement.play();
-    const values = this.library().map(() => 0);
-    this.currentTranslation.set([...values]);
-    this.leftStop.set([...values]);
-    const scrollDefaultData = values.map((v) => {
-      return {
-        delta: 0,
-        track: 0,
-        indicator: 0,
-        trackPercent: 0,
-      };
-    });
-    this.videoId = this.library()[0].videos[0].id ?? 0;
-    this.scrollData.set([...scrollDefaultData]);
     this.route.paramMap.subscribe({
       next: (value) =>
         this.user.logIn({
@@ -124,131 +70,48 @@ export class VideoOffer implements OnInit {
     });
   }
 
-  ngAfterViewInit() {
-    this.leftStop.update((values) =>
-      values.map((v, i) => {
-        const track =
-          this.genreRowTrack?.get(i)?.nativeElement.clientWidth ?? 0;
-        const indicator = this.genreRow?.get(i)?.nativeElement.scrollWidth ?? 0;
-        // console.log('delta: ', track - indicator, track, indicator);
-        const scrollData = {
-          delta: track - indicator,
-          track: track,
-          indicator: indicator,
-          trackPercent: Math.round((track / indicator) * 100),
-        };
-        console.log('scroll data: ', scrollData);
-        this.scrollData()[i] = scrollData;
-
-        return track - indicator;
-      })
-    );
-
-    console.log('scroll data: ', this.scrollData());
-
-    // mousewheel events
-    // this.genreRow?.forEach((r, i) => {
-    //   const row = r.nativeElement;
-    //   row.addEventListener('mousewheel', (event) => this.onScroll(event, i), {
-    //     passive: false,
-    //   });
-    // });
+  /**
+   * Set a random video.
+   */
+  private setRandomVideo() {
+    const id = this.getRandomVideoId();
+    this.video.update(() => this.newVideos()[id]);
   }
 
-  switchScrollArea() {
-    this.scrollArea.update((value) => !value);
+  /**
+   * Get a random video id.
+   * @returns - The random video id.
+   */
+  private getRandomVideoId() {
+    const lastId = this.newVideos().length - 1;
+    return Math.round(Math.random() * lastId);
   }
 
-  onScroll(event: Event, index: number) {
-    console.log('event: ', index);
-    event.preventDefault();
-    const row = this.genreRow?.get(index)?.nativeElement;
-    const wheelEvent = event as WheelEvent;
-
-    if (wheelEvent.deltaY < 0) {
-      // if (this.currentTranslation()[index] < 0) {
-      //   event.preventDefault();
-      // }
-      const test = this.currentTranslation()[index] + 213 + 16;
-      this.currentTranslation()[index] = Math.min(0, test);
-      row?.setAttribute(
-        'style',
-        `transform: translateX(${
-          this.currentTranslation()[index]
-        }px); transition: transform 300ms ease-in-out`
-      );
-    }
-    if (wheelEvent.deltaY > 0) {
-      // if (this.currentTranslation()[index] > this.leftStop()[index]) {
-      //   event.preventDefault();
-      // }
-      const delta = this.leftStop()[index];
-      const test = this.currentTranslation()[index] - 213 - 16;
-      this.currentTranslation()[index] = Math.max(delta, test);
-      row?.setAttribute(
-        'style',
-        `transform: translateX(${
-          this.currentTranslation()[index]
-        }px); transition: transform 300ms ease-in-out`
-      );
-    }
-  }
-
+  /**
+   * Redirect the user to the video player on click.
+   */
   onPlay() {
+    const url = this.getVideoPlayerUrl();
+    this.router.navigateByUrl(url);
+  }
+
+  /**
+   * Get a video player URL.
+   * @returns The video player URL.
+   */
+  private getVideoPlayerUrl() {
     const token = this.auth.getToken();
-    const id = this.getVideoId();
-    this.router.navigateByUrl(`/video/player/${token}/${id}`);
+    const id = this.id();
+    return `/video/player/${token}/${id}`;
   }
 
-  setVideo(video: Video) {
-    this.setVideoId(video.id);
-    this.video?.set(video);
-
-    this.preview?.nativeElement.setAttribute('src', video.previewClip);
-    this.title.set(video.title);
-    this.description.set(video.description);
-
-    console.log('genre row: ', this.genreRow);
-  }
-
-  getVideoId() {
-    return this.videoId.toString();
-  }
-
-  setVideoId(id: number) {
-    this.videoId = id;
-  }
-
-  // set slide limit - check
-  onToRight($index: number) {
-    console.log('to right: ', $index);
-    const row = this.genreRow?.get($index);
-    console.log('row from left: ', row);
-
-    const test = this.currentTranslation()[$index] + 213 + 16;
-    this.currentTranslation()[$index] = Math.min(0, test);
-    row?.nativeElement.setAttribute(
-      'style',
-      `transform: translateX(${
-        this.currentTranslation()[$index]
-      }px); transition: transform 300ms ease-in-out`
-    );
-  }
-
-  // set lide limit ...
-  onToLeft($index: number) {
-    console.log('to left: ', $index);
-    const row = this.genreRow?.get($index);
-    console.log('row from right: ', row);
-
-    const delta = this.leftStop()[$index];
-    const test = this.currentTranslation()[$index] - 213 - 16;
-    this.currentTranslation()[$index] = Math.max(delta, test);
-    row?.nativeElement.setAttribute(
-      'style',
-      `transform: translateX(${
-        this.currentTranslation()[$index]
-      }px); transition: transform 300ms ease-in-out`
-    );
+  /**
+   * Update the video preview on click.
+   * @param video - The video to be set.
+   */
+  onPreview(video: Video) {
+    this.video.set(video);
+    this.preview?.nativeElement.load();
+    this.scroller.scrollToPosition([0, 0], { behavior: 'smooth' });
   }
 }
